e01f5c01914c13171141165a0c185ee71c800122
MaxV <maximus@mv-tower.(none)>
2012-11-21 02:13:55 +0000

    Added skeletons for data classes. Hey, it's something at least

---
+++ codes/data/I_observer.py
@@ -0,0 +1,21 @@
+
+class i_observer(object):
+
+    """
+     
+
+    :version:
+    :author:
+    """
+
+    def update(self):
+        """
+         
+
+        @return  :
+        @author
+        """
+        pass
+
+
+
